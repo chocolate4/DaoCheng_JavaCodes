9edea71e90ff04d478fa84c9c1df5cfe5730230a
daocheng <806186987@qq.com>
2022-05-29 17:25:02 +0800

    work06

--- 04Concurrency/src/main/java/com/daocheng/Test.java
+++ 04Concurrency/src/main/java/com/daocheng/Test.java
@@ -3,11 +3,14 @@ package com.daocheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @SpringBootApplication
 public class Test {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test.class, args);
+		ConcurrentHashMap currentHashMap = new ConcurrentHashMap();
 	}
 
 }
