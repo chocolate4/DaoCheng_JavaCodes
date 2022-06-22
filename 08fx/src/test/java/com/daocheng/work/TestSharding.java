d1b0ba07d45b37dabda0170605c9bc447abc59fe
daocheng <806186987@qq.com>
2022-06-22 11:50:42 +0800

    work

--- 08fx/src/test/java/com/daocheng/work/TestSharding.java
+++ 08fx/src/test/java/com/daocheng/work/TestSharding.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 @SpringBootTest
@@ -23,22 +24,25 @@ public class TestSharding {
 
     @Test
     void test() throws ParseException {
-        Order order = new Order();
-        order.setOrderId(33000000010L);
-        order.setUserId(330006L);
-        order.setOrderStatus("已下单");
-        order.setProductCount(1);
-        order.setPrice(100.0);
-        order.setOrderCreateTime(1655136000L);
-        order.setOrderFinishTime(1655136000L);
-        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
-        Date createTime = simpleDateFormat1.parse("2022-06-15 00:00:00");
-        order.setCreateTime(createTime);
-        order.setUpdateTime(createTime);
-        orderMapper.insert(order);
-//        service.save(order);
-//        List<Order> orders = orderMapper.selectList(null);
-//        System.out.println(orders.size());
+        final LinkedList<Order> orders = new LinkedList<>();
+        for(int j = 1; j < 100; j++) {
+            for(int i = 0; i < 100; i++) {
+                Order order = new Order();
+                order.setOrderId(370000000L + j*100 + i);
+                order.setUserId(370000L + j);
+                order.setOrderStatus("已下单");
+                order.setProductCount(1);
+                order.setPrice(100.0);
+                order.setOrderCreateTime(1655136000L);
+                order.setOrderFinishTime(1655136000L);
+                SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
+                Date createTime = simpleDateFormat1.parse("2022-06-15 00:00:00");
+                order.setCreateTime(createTime);
+                order.setUpdateTime(createTime);
+                orders.add(order);
+            }
+        }
+        service.saveBatch(orders);
 //        service.removeById(3300000000L);
 //        service.updateById(order);
     }
